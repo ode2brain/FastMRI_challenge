dba9ec55bc1dca7565b8d1829b9628de0ee563ce
PFRC members <neuralode@gmail.com>
2024-07-14 08:18:57 +0900

    Finished Running Varnet Train

--- leaderboard_eval.sh
+++ leaderboard_eval.sh
@@ -1,3 +1,3 @@
 python leaderboard_eval.py \
   -lp '/home/Data/leaderboard' \
-  -yp '/root/result/test_Varnet/reconstructions_leaderboard'
+  -yp '../result/test_Varnet/reconstructions_leaderboard'
