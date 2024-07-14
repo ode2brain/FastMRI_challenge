ea6819b966d50cb28450dc6ad86a6794df3d5061
PFRC members <neuralode@gmail.com>
2024-07-14 17:10:10 +0900

    Training...

--- leaderboard_eval.sh
+++ leaderboard_eval.sh
@@ -1,3 +1,3 @@
 python leaderboard_eval.py \
   -lp '/home/Data/leaderboard' \
-  -yp '../result/test_Varnet/reconstructions_leaderboard'
+  -yp '/home/neuralode/result/test_Varnet/reconstructions_val'
